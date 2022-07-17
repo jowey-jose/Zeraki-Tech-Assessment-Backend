d75a06b1bd9fc6c12235adc71fc572ebd0b1e25c
Jowey-jose <josephmbuku17@gmail.com>
2022-07-17 20:49:16 +0300

    Add Project Folder Structure

---
+++ src/main/java/com/zeraki/zerakitechassessmentbackend/ZerakiTechAssessmentBackendApplication.java
@@ -0,0 +1,14 @@
+package com.zeraki.zerakitechassessmentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerakiTechAssessmentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerakiTechAssessmentBackendApplication.class, args);
+
+    }
+
+}
